3a748e8f492ea09c9136012bb26473d71ada2ce3
LOGTHEMIN\christian.johansen <christian.johansen@makingwaves.com>
2017-09-21 15:42:32 +0200

    added hasClicked state

--- src/spar/pages/Wheel.js
+++ src/spar/pages/Wheel.js
@@ -3,6 +3,7 @@ import HomeHeader from "../HomeHeader"
 import { App } from "../../common/modules/app/markup"
 
 export default class Home extends PureComponent {
+    state = { hasClicked: false };
     render() {
         return (
             <App.Top>
@@ -13,7 +14,7 @@ export default class Home extends PureComponent {
                     <div className="container">
                         <p>Wheel</p>
                         <img className="wheelArrow" src="/images/arrow_down_red.png" />
-                        <img className="spinthewheel" src="/images/wheel_test.jpg" />
+                        <img className={this.state.hasClicked ? "spinthewheel wheel" : "waiting wheel"} onClick={() => this.setState({ hasClicked: true })} src="/images/wheel_test.jpg" />
                     </div>
                 </App.Main>
             </App.Top>
